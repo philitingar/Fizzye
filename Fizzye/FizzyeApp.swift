942a5e9cb1939e3d9d819f3d96c135ebce452add
Timea Bartha <timea.bartha.93@gmail.com>
2024-12-09 16:23:07 +0100

    Initial Commit

---
+++ Fizzye/FizzyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzyeApp.swift
+//  Fizzye
+//
+//  Created by Timea Bartha on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FizzyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
