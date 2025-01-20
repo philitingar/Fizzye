23716e8d69dc1fb084005a694b4648a354589a84
Timea Bartha <timea.bartha.93@gmail.com>
2025-01-20 12:02:35 +0100

    Added text and textfield
    Started creating the app base.

--- Fizzye/ContentView.swift
+++ Fizzye/ContentView.swift
@@ -6,18 +6,44 @@
 //
 
 import SwiftUI
-
+extension Color {
+    static let pepperRed = Color(red: 137 / 255, green: 0 / 255, blue: 36 / 255)
+}
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center,  spacing: 1) {
+                Image("pepper")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 400, maxHeight: 400)
+                    .opacity(0.5)
+                Text("This app only works with DrPepper produced in the USA.")
+                    .font(.title3)
+                    .foregroundColor(.pepperRed)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .opacity(0.7)
+                    .padding(.horizontal, 15)
+            }.padding()
+            VStack {
+                TextField("Enter first 4 digits here, ex: A023", text: .constant(""))
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 20)
+                Spacer()
+            }
         }
-        .padding()
+         
     }
+    // #890024
 }
+
 
 #Preview {
     ContentView()
