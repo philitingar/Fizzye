1f460759399087d2a1d74664abada8a9c13f4c99
Timea B <timea.bartha.93@gmail.com>
2025-01-27 22:25:43 +0100

    Updated the UI with a ScrollView

--- Fizzye/Views/ContentView.swift
+++ Fizzye/Views/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
     @State private var errorMessage: String?
     let options = ["Sugary", "Zero", "Diet"]
     private let validationRule = IncrementalValidationRule()
-    let drPepperGroupItems = ["Dr Pepper", "Snapple", "RC Cola", "A&W", "7 Up", "Schweppes", "Sunkist", "Canada Dry", "Big Red", "Mott's", "Vernors", "Hawaiian Punch", "Nehi", "Squirt"]
+    let drPepperGroupItems = ["Unselectable List:","Dr Pepper", "Snapple", "RC Cola", "A&W", "7 Up", "Schweppes", "Sunkist", "Canada Dry", "Big Red", "Mott's", "Vernors", "Hawaiian Punch", "Nehi", "Squirt"]
     var body: some View {
         ZStack {
             Color.veryDarkGrey
@@ -32,132 +32,152 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .opacity(0.4)
-            VStack {
-                HStack {
-                    Text("Dr Pepper Snapple Group which can include:")
-                        .font(.system(size: 16, weight: .bold))
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(.bottom, -3)
-                        .layoutPriority(1)
-                        .minimumScaleFactor(0.5)
-                    Picker("Dr Pepper Snapple Group which can include:", selection: .constant("")) {
-                        ForEach(drPepperGroupItems, id: \.self) { option in
-                            Text(option)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .frame(width: 10)
-                    .foregroundColor(Color.clear)
-                    .overlay(
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 16))
-                            .bold()
+            ScrollView() {
+                VStack {
+                    HStack {
+                        Text("Dr Pepper Snapple Group which can include:")
+                            .font(.system(size: 16, weight: .bold))
                             .foregroundColor(.white)
-                            .background(Color.veryDarkGrey)
+                            .bold()
                             .padding(.bottom, -3)
-                        
-                    )
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                HStack {
-                    ForEach(0..<options.count, id: \.self) { index in
-                        Button {
-                            selectedOption = index
-                        } label: {
-                            Text(self.options[index])
-                                .font(.system(size: 18, weight: .bold))
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .background(
-                                    Group {
-                                        if index == 0 {
-                                            self.selectedOption == index ? Color.pepperRed : Color.lighterGrey
-                                        } else if index == 1 {
-                                            self.selectedOption == index ? Color.black : Color.lighterGrey
-                                        } else if index == 2 {
-                                            self.selectedOption == index ? Color.gray : Color.lighterGrey
-                                        }
-                                    }
-                                )
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .overlay(
-                                    Group {
-                                        if selectedOption == index {
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .stroke(Color.pepperRed, lineWidth: 2)
-                                        }
-                                    }
-                                )
+                            .layoutPriority(1)
+                            .minimumScaleFactor(0.5)
+                        Picker("Dr Pepper Snapple Group which can include:", selection: .constant("")) {
+                            ForEach(drPepperGroupItems, id: \.self) { option in
+                                Text(option)
+                            }
                         }
-                    }
-                }
-                .padding(.top, 10)
-                .padding(.horizontal, 15)
-                .padding(.bottom, 5)
-                Text("This calculator only works with beverages produced in the USA.")
-                    .foregroundStyle(.gray)
-                    .font(.footnote)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity)
-                
-                TextField("Enter first 5 digits of code here, ex: A1234", text: $inputText)
-                    .padding()
-                    .background(Color.gray)
-                    .foregroundStyle(.white)
-                    .cornerRadius(10)
-                    .padding(.horizontal, 15)
-                    .padding(.top, 5)
-                    .onChange(of: inputText) { oldvalue, newValue in
-                        let uppercasedValue = newValue.uppercased()
-                        let filteredValue = uppercasedValue.filter { $0.isLetter || $0.isNumber }
-                        inputText = String(filteredValue.prefix(5))
-                        
-                        if !inputText.isEmpty {
-                            errorMessage = validationRule.validate(filteredValue)
-                        } else {
-                            errorMessage = nil
-                        }
-                       
-                    }
-                if let errorMessage = errorMessage {
-                    Text(errorMessage)
-                        .font(.caption)
-                        .foregroundColor(.red)
-                        .padding(.horizontal, 15)
-                }
-                Button {
-                    if selectedOption == -1 {
-                        showAlert = true
-                        alertMessage = "Please select the type of drink before calculating."
-                        inputText = inputText
-                    } else {
-                        let monthCode = String(inputText.prefix(1))
-                        let dayOfTheYearString = String(inputText.suffix(3))
-                        if vm.isvalidMonthAndDay(code: monthCode, dayOfYearString: dayOfTheYearString) {
-                            expirationDate = vm.calculateExpirationDate(code: inputText, selectedOption: selectedOption)
-                            isSheetPresented = true
-                        } else {
-                            showAlert = true
-                            alertMessage = "The code you entered is incorrect. Make sure you put in the correct code."
-                        }
-                    }
-                } label: {
-                    Text(inputText.isEmpty || errorMessage != nil || inputText.count < 5 ? "Enter details" : "Calculate Expiration")
-                        .font(.system(size: 18, weight: .bold))
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.darkerPepperRed)
-                        .cornerRadius(10)
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 10)
+                        .foregroundColor(Color.clear)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.pepperRed, lineWidth: 2)
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 16))
+                                .bold()
+                                .foregroundColor(.white)
+                                .background(Color.veryDarkGrey)
+                                .padding(.bottom, -3)
+                            
                         )
-                        .padding(.top, 20)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    HStack {
+                        ForEach(0..<options.count, id: \.self) { index in
+                            Button {
+                                selectedOption = index
+                            } label: {
+                                Text(self.options[index])
+                                    .font(.system(size: 18, weight: .bold))
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(
+                                        Group {
+                                            if index == 0 {
+                                                self.selectedOption == index ? Color.pepperRed : Color.lighterGrey
+                                            } else if index == 1 {
+                                                self.selectedOption == index ? Color.black : Color.lighterGrey
+                                            } else if index == 2 {
+                                                self.selectedOption == index ? Color.gray : Color.lighterGrey
+                                            }
+                                        }
+                                    )
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        Group {
+                                            if selectedOption == index {
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.pepperRed, lineWidth: 2)
+                                            }
+                                        }
+                                    )
+                            }
+                        }
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+                    .padding(.bottom, 5)
+                    Text("This calculator only works with beverages produced in the USA.")
+                        .foregroundStyle(.gray)
+                        .font(.footnote)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
+                    
+                    TextField("Enter first 5 digits of code here, ex: A1234", text: $inputText)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundStyle(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 15)
+                        .padding(.top, 5)
+                        .onChange(of: inputText) { oldvalue, newValue in
+                            let uppercasedValue = newValue.uppercased()
+                            let filteredValue = uppercasedValue.filter { $0.isLetter || $0.isNumber }
+                            inputText = String(filteredValue.prefix(5))
+                            
+                            if !inputText.isEmpty {
+                                errorMessage = validationRule.validate(filteredValue)
+                            } else {
+                                errorMessage = nil
+                            }
+                            
+                        }
+                    if let errorMessage = errorMessage {
+                        Text(errorMessage)
+                            .font(.caption)
+                            .foregroundColor(.red)
+                            .padding(.horizontal, 15)
+                    }
+                    Button {
+                        if selectedOption == -1 {
+                            showAlert = true
+                            alertMessage = "Please select the type of drink before calculating."
+                            inputText = inputText
+                        } else {
+                            let monthCode = String(inputText.prefix(1))
+                            let dayOfTheYearString = String(inputText.suffix(3))
+                            if vm.isvalidMonthAndDay(code: monthCode, dayOfYearString: dayOfTheYearString) {
+                                expirationDate = vm.calculateExpirationDate(code: inputText, selectedOption: selectedOption)
+                                isSheetPresented = true
+                            } else {
+                                showAlert = true
+                                alertMessage = "The code you entered is incorrect. Make sure you put in the correct code."
+                            }
+                        }
+                    } label: {
+                        Text(inputText.isEmpty || errorMessage != nil || inputText.count < 5 ? "Enter details" : "Calculate Expiration")
+                            .font(.system(size: 18, weight: .bold))
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.darkerPepperRed)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.pepperRed, lineWidth: 2)
+                            )
+                            .padding(.top, 20)
+                    }
+                    .disabled(inputText.isEmpty || errorMessage != nil || inputText.count < 5)
                 }
-                .disabled(inputText.isEmpty || errorMessage != nil || inputText.count < 5)
+                .sheet(isPresented: $isSheetPresented) {
+                    BottomSheetView(expirationDate: expirationDate, code: inputText, selectedOption: selectedOption)
+                        .presentationDetents([.height(450)])
+                        .presentationDragIndicator(.visible)
+                        .presentationBackground(Color.pepperRed)
+                        .cornerRadius(80)
+                }
+                .onChange(of: isSheetPresented) { oldvalue, isPresented in
+                    if !isPresented {
+                        inputText = ""
+                        selectedOption = -1
+                    }
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+            }
+            VStack{
                 Spacer()
                 Text("If you need help, or have a suggestion for a beverage that isn't listed, please fill out our form:")
                     .foregroundStyle(.gray)
@@ -174,22 +194,6 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color.gray, lineWidth: 1)
                     )
-            }
-            .sheet(isPresented: $isSheetPresented) {
-                BottomSheetView(expirationDate: expirationDate, code: inputText, selectedOption: selectedOption)
-                    .presentationDetents([.height(450)])
-                    .presentationDragIndicator(.visible)
-                    .presentationBackground(Color.pepperRed)
-                    .cornerRadius(80)
-            }
-            .onChange(of: isSheetPresented) { oldvalue, isPresented in
-                if !isPresented {
-                    inputText = ""
-                    selectedOption = -1
-                }
-            }
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
         }
     }
